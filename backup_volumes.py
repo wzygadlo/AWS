5252ae529acde4f86f1cf9b31af864c9ecdb3f5d
Wojtek <wzygadlo@users.noreply.github.com>
2017-06-20 00:28:49 +0100

    Update backup_volumes.py

--- backup_volumes.py
+++ backup_volumes.py
@@ -5,5 +5,5 @@ ec2 = boto3.resource('ec2')
 
 for volume in ec2.volumes.all():
     vol_id = volume.id
-    description = "backup-%s" %(vol_id)
+    description = "backup-{}" .format(vol_id) 
     ec2.create_snapshot(VolumeId=vol_id, Description=description)
